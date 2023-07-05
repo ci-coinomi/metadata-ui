68ffc1e60463beb5867b0e4caa7b9424ab502fc7
PavelEucalyptuslabs <pavel@eucalyptuslabs.com>
2023-07-05 10:07:10 +0300

    web-736 questions page protected

--- middleware/02.protectedRoutes.global.js
+++ middleware/02.protectedRoutes.global.js
@@ -30,6 +30,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     return navigateTo("/forbidden");
   }
 
+  if (to.name === "questions" && !rolesArr?.includes("ROLE_ADMIN")) {
+    return navigateTo("/no-access");
+  }
+
   if (
     to.name === "users" &&
     rolesArr?.includes("ROLE_ADMIN") &&
