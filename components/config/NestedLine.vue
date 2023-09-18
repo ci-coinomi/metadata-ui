ad97a9e205d1d96abc84c5fe5f97378d692ab1b2
PavelEucalyptuslabs <pavel@eucalyptuslabs.com>
2023-09-18 16:08:19 +0300

    web-813 arrays logic only for providers & isConfigUpdated update

--- components/config/NestedLine.vue
+++ components/config/NestedLine.vue
@@ -1,70 +1,76 @@
 <template>
-  <p
-    v-if="
-      props.configNestedObject &&
-      !props.configNestedObject.hasOwnProperty('@type')
-    "
-    class="text-gray-600"
-  >
-    <span> Object </span>
-    <span v-if="Object.entries(props.configNestedObject).length === 0">
-      (empty)
-    </span>
-  </p>
   <div
-    v-for="(value, key) in props.configNestedObject"
-    :key="key"
-    :class="
-      isObject(value) || Array.isArray(value) ? 'items-start' : 'items-center'
-    "
-    class="flex py-1 gap-4 rounded-sm"
+    class="p-2 flex flex-col gap-2 w-full rounded-sm"
+    :class="configBorderStyle"
   >
     <p
-      :class="
-        isObject(value) || Array.isArray(value)
-          ? 'text-gray-600'
-          : 'text-gray-400'
+      v-if="
+        props.configNestedObject &&
+        !props.configNestedObject.hasOwnProperty('@type')
       "
+      class="text-gray-600"
     >
-      {{ key }}
+      <span> Object </span>
+      <span v-if="Object.entries(props.configNestedObject).length === 0">
+        (empty)
+      </span>
     </p>
-
-    <template v-if="isObject(value)">
-      <div
-        class="p-2 flex flex-col gap-2 w-full border border-gray-500 rounded-sm"
+    <div
+      v-for="(value, key) in props.configNestedObject"
+      :key="key"
+      :class="
+        isObject(value) || Array.isArray(value) ? 'items-start' : 'items-center'
+      "
+      class="flex py-1 gap-4 rounded-sm"
+    >
+      <p
+        :class="
+          isObject(value) || Array.isArray(value)
+            ? 'text-gray-600'
+            : 'text-gray-400'
+        "
       >
+        {{ key }}
+      </p>
+
+      <template v-if="isObject(value)">
         <configNestedLine
           :isCloned="isCloned"
           :configNestedObject="value"
           :configUpdateTrigger="props.configUpdateTrigger"
         />
-      </div>
-    </template>
+      </template>
 
-    <UiSwitcher
-      v-else-if="typeof value === 'boolean'"
-      :value="configNestedObject[key]"
-      @update:value="(data) => (configNestedObject[key] = data)"
-    />
+      <UiSwitcher
+        v-else-if="typeof value === 'boolean'"
+        :value="configNestedObject[key]"
+        @update:value="(data) => (configNestedObject[key] = data)"
+      />
 
-    <template v-else-if="Array.isArray(value)">
-      <div
-        class="p-2 flex flex-col gap-2 w-full border border-gray-500 rounded-sm"
-      >
-        <configNestedArray
+      <template v-else-if="Array.isArray(value)">
+        <configNestedProvider
+          v-if="key === 'nodeProviders' || key === 'apiProviders'"
           :isCloned="isCloned"
           :configNestedObject="value"
           :configUpdateTrigger="props.configUpdateTrigger"
         />
-      </div>
-    </template>
+        <UiInputField
+          v-else
+          :value="configNestedObject[key]"
+          type="text"
+          @input="
+            (data) => (configNestedObject[key] = data.target.value.split(','))
+          "
+        />
+      </template>
 
-    <UiInputField
-      v-else
-      v-model="configNestedObject[key]"
-      type="text"
-      :disabled="isFieldDisabled(key)"
-    />
+      <UiInputField
+        v-else
+        v-model="configNestedObject[key]"
+        type="text"
+        :disabled="isFieldDisabled(key)"
+      />
+    </div>
   </div>
 </template>
 
@@ -73,6 +79,17 @@ const props = defineProps({
   configNestedObject: Object,
   isCloned: Boolean,
   configUpdateTrigger: Number,
+});
+
+const defaultNestedObject = ref(cleared(props.configNestedObject));
+
+const isConfigUpdated = ref(false);
+
+const configBorderStyle = computed(() => {
+  if (props.configNestedObject?.["@type"]) return "";
+  return isConfigUpdated.value
+    ? "border border-[#d38b32]"
+    : "border border-gray-400";
 });
 
 const isFieldDisabled = (key) => {
@@ -88,4 +105,27 @@ const isFieldDisabled = (key) => {
 const isObject = (value) => {
   return typeof value === "object" && value !== null && !Array.isArray(value);
 };
+
+watch(
+  () => props.configNestedObject,
+  () => {
+    !areObjectsEqual(
+      cleared(defaultNestedObject.value),
+      cleared(props.configNestedObject),
+    )
+      ? (isConfigUpdated.value = true)
+      : (isConfigUpdated.value = false);
+  },
+  {
+    deep: true,
+  },
+);
+
+watch(
+  () => props.configUpdateTrigger,
+  () => {
+    defaultNestedObject.value = cleared(props.configNestedObject);
+    isConfigUpdated.value = false;
+  },
+);
 </script>
