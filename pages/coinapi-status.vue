480789a691827f38fcf8aff11bc43a4962ef39bd
PavelEucalyptuslabs <pavel@eucalyptuslabs.com>
2023-10-05 12:39:14 +0300

    web-828 chains height has been added

--- pages/coinapi-status.vue
+++ pages/coinapi-status.vue
@@ -9,50 +9,11 @@
       </template>
 
       <template v-else>
-        <article
+        <CoinApiListItem
           v-for="chain in chainsList"
           :key="chain.eucId"
-          class="flex border border-black p-6 rounded-xl w-full items-center gap-3"
-        >
-          <h2 class="w-1/5">{{ chain.name }}</h2>
-          <div class="flex flex-1 flex-col">
-            <div class="flex flex-wrap gap-1">
-              <h3 class="text-gray-400">Node Providers:</h3>
-              <p v-if="chain.nodeProviders.length === 0" class="text-gray-400">
-                Does not exist
-              </p>
-              <template v-else>
-                <p
-                  v-for="(provider, index) in chain.nodeProviders"
-                  :key="provider"
-                >
-                  {{ provider
-                  }}<span v-if="index + 1 < chain.nodeProviders.length">,</span
-                  ><span v-else>.</span>
-                </p>
-              </template>
-            </div>
-            <div class="flex flex-wrap gap-1">
-              <h3 class="text-gray-400">Api Providers:</h3>
-              <p v-if="chain.apiProviders.length === 0" class="text-gray-400">
-                Does not exist
-              </p>
-              <template v-else>
-                <p
-                  v-for="(provider, index) in chain.apiProviders"
-                  :key="provider"
-                >
-                  {{ provider
-                  }}<span v-if="index + 1 < chain.apiProviders.length">,</span
-                  ><span v-else>.</span>
-                </p>
-              </template>
-            </div>
-          </div>
-          <div class="w-1/5">
-            <CoinApiBlockHeight :chainId="chain.eucId" />
-          </div>
-        </article>
+          :chain="chain"
+        />
       </template>
     </section>
   </main>
